285cd2aca110f6c100666e0338b85760dd5d6c40
truckjumperdude <102482920+truckjumperdude@users.noreply.github.com>
2023-12-15 12:55:38 -0600

    Update review.php

--- Jem/review.php
+++ Jem/review.php
@@ -1,3 +1,10 @@
 <?php
 $reviewtext = $_POST['reviewtext'];
+$reviewtext = "<p class=\"p\">" + $reviewtext + "</p>\n<br>";
+$file = fopen("review.html", "w") or die("lol");
+fwrite($file, $reviewtext);
+fclose($file);
 ?>
+<HTML lang="en">
+<a href="./truck.html">Return to the beautiful truck</a>
+</html>
